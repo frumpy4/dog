8342670e544c28d424ae11c10df6c1c7b0e2708e
frumpy4 <frumpy4@gmail.com>
2016-02-15 12:18:01 -0700

    files

---
+++ something.js
@@ -0,0 +1,25 @@
+var r = false
+var d = 360
+var i = 1
+var update_des = false
+function rotate() {
+    if (r) {
+        if (update_des) {
+            document.getElementById("description").innerHTML = "speed = "+i
+            update_des = false
+        }
+        document.getElementById("dog").style = "transform: rotate("+d+"deg)";
+        d -= i;
+        if (d <= 0) {
+            d += 360;
+        }
+        requestAnimationFrame(rotate);
+    } else {
+        i *= 1.1
+        update_des = true
+    }
+}
+function toggle() {
+    r = !r
+    rotate();
+}
