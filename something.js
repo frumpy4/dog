e012567b61fe305c90b89bb306afb423d248a268
frumpy4 <frumpy4@gmail.com>
2016-02-15 13:01:15 -0700

    no more pausing, add gitignore

--- something.js
+++ something.js
@@ -1,25 +1,19 @@
-var r = false
 var d = 360
 var i = 1
 var update_des = false
 function rotate() {
-    if (r) {
-        if (update_des || i == 1) {
-            document.getElementById("description").innerHTML = "speed = "+i
-            update_des = false
-        }
-        document.getElementById("dog").style = "transform: rotate("+d+"deg)";
-        d -= i;
-        if (d <= 0) {
-            d += 360;
-        }
-        requestAnimationFrame(rotate);
-    } else {
-        i *= 1.1
-        update_des = true
+    if (update_des || i == 1) {
+        document.getElementById("description").innerHTML = "speed = "+i
+        update_des = false
     }
+    document.getElementById("dog").style = "transform: rotate("+d+"deg)";
+    d -= i;
+    if (d <= 0) {
+        d += 360;
+    }
+    requestAnimationFrame(rotate);
 }
-function toggle() {
-    r = !r
-    rotate();
+function increase() {
+    i *= 1.1
+    update_des = true
 }
