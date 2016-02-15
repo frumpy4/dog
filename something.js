c3069a1bc0fc7c3eb1ed2d7a0975d992f7a45ed4
frumpy4 <frumpy4@gmail.com>
2016-02-15 14:09:29 -0700

    rewrote js

--- something.js
+++ something.js
@@ -1,24 +1,37 @@
-var r = false
-var d = 360
-var i = 1
-var update_des = false
+var deg = 360
+var speed = 0
+var prev_speed = 0
+var des = ""
+var max = false
+var max_speed = 200000
+
 function rotate() {
-    if (update_des || i == 1) {
-        document.getElementById("description").innerHTML = "speed = "+i
-        update_des = false
+    max = speed >= max_speed
+    if (prev_speed != speed) {
+        prev_speed = speed
+        des = "speed = "+speed
+        if (max) {
+            des = "the dog is going fast enough please spare him"
+        }
+        document.getElementById("description").innerHTML = des
     }
-    document.getElementById("dog").style = "transform: rotate("+d+"deg)";
-    d -= i;
-    if (d <= 0) {
-        d += 360;
+    document.getElementById("dog").style = "transform: rotate("+deg+"deg)"
+    deg -= speed
+    while (deg <= 0) {
+        deg += 360
     }
-    requestAnimationFrame(rotate);
+    requestAnimationFrame(rotate)
 }
+
 function increase() {
-    i *= 1.1
-    update_des = true
-    if (!r) {
-        r = true
-        requestAnimationFrame(rotate);
+    if (max) {
+        console.log("you monster")
+        return;
+    }
+    if (speed == 0) {
+        speed++
+    } else {
+        speed *= 1.1
     }
 }
+requestAnimationFrame(rotate)
