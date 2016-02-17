7a1623b23e940654c31a311ddd03c93e45bdb082
frumpy4 <frumpy4@gmail.com>
2016-02-16 18:13:41 -0700

    added controls, reorganized html a bit

--- something.js
+++ something.js
@@ -1,26 +1,33 @@
 var deg = 360
 var speed = 0
 var prev_speed = 0
-var des = ""
 var max = false
 var max_speed = 100000000
 
+var controls_open = false
+
 function rotate() {
     max = speed >= max_speed
-    if (prev_speed != speed) {
-        prev_speed = speed
-        des = "speed = "+(speed.toFixed(3).replace(/\B(?=(\d{3})+(?!\d))/g, ","))
-        if (max) {
-            des = "the dog is going fast enough please spare him"
-        }
-        document.getElementById("description").innerHTML = des
+    if (Number(document.getElementById("speed_input").value) == prev_speed) {
+        document.getElementById("speed_input").value = speed
     }
+    updateDescription(false)
     document.getElementById("dog").style = "transform: rotate("+deg+"deg)"
     deg -= speed
     while (deg <= 0) {
         deg += 360
     }
     requestAnimationFrame(rotate)
+}
+
+function updateDescription(fromControls) {
+    if (prev_speed != speed || fromControls) {
+        prev_speed = speed
+        document.getElementById("description").innerHTML = "speed = "+(speed.toFixed(3).replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+    }
+    if (max) {
+        document.getElementById("description").innerHTML = "the dog is going fast enough please spare him"
+    }
 }
 
 function increase() {
@@ -37,6 +44,28 @@ function increase() {
 
 function rand(min, max) {
     return Math.random()*(max-min)+min;
+}
+
+function toggleControls() {
+    if (controls_open) {
+        document.getElementById("controls_button").innerHTML = "+"
+        document.getElementById("controls").style.display = "none"
+    } else {
+        document.getElementById("controls_button").innerHTML = "-"
+        document.getElementById("controls").style.display = ""
+    }
+    controls_open = !controls_open
+}
+
+function updateVars() {
+    speed = Number(document.getElementById("speed_input").value)
+    max_speed = Number(document.getElementById("maxspeed_input").value)
+    max = speed >= max_speed
+    updateDescription(true)
+}
+
+function onLoad() {
+    document.getElementById("maxspeed_input").value = max_speed
 }
 
 requestAnimationFrame(rotate)
